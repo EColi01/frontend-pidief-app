af1e8d9780e11f16d8ef19a6bca6bd682501a3f1
EColi01 <carlos.amtzg1@gmail.com>
2025-06-30 15:59:34 -0600

    v1.2.3

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import './index.css';
+
 import App from "./App";           // Página principal
 import UnirPDF from "./UnirPDF";   // Página para unir PDFs
 
