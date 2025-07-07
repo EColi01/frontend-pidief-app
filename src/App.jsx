ae2559ccd3e6685c79d9d0144e9f2adb321e891b
EColi01 <carlos.amtzg1@gmail.com>
2025-07-06 19:05:55 -0600

    v1.4.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-<div className="bg-blue-500 text-white p-4 rounded shadow">
-  Tailwind está funcionando kkk 🚀
-</div>
+import './index.css'; // 👈 Esta línea debe estar arriba de todo
+
 function App() {
   return (
     <div className="bg-blue-500 text-white p-4 rounded shadow">
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+import './index.css'; // 👈 Esta línea debe estar arriba de todo
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
