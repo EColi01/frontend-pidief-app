d7206ab7bc01436f334365d822b757bac7fe6beb
EColi01 <carlos.amtzg1@gmail.com>
2025-07-06 18:58:02 -0600

    v1.4.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 <div className="bg-blue-500 text-white p-4 rounded shadow">
   Tailwind está funcionando kkk 🚀
 </div>
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded shadow">
+      Tailwind está funcionando kkk 🚀
+    </div>
+  );
+}
+
+export default App;
