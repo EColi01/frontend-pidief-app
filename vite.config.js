3c6de5f5dc1309d617a13673833591d7a6dfe6c6
EColi01 <carlos.amtzg1@gmail.com>
2025-07-11 15:27:12 -0600

    v1.4.13

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-});
