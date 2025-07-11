3c9c7cbc144ea20d6b0238698f1ded585b195a4c
EColi01 <carlos.amtzg1@gmail.com>
2025-07-11 14:03:17 -0600

    v1.4.8

--- src/pages/UnirPDF.jsx
+++ src/pages/UnirPDF.jsx
@@ -115,7 +115,7 @@ export default function UnirPDF() {
           onClick={handleEnviar}
           className="mt-4 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded shadow"
         >
-          Unir PDFs
+          Unir PDFs!
         </button>
       )}
     </div>
